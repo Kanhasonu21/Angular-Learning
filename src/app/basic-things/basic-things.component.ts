03a377877b6d7a48de25d7e1e95eb8f5706eb3ca
Kanhaiya Kumar <kanhaiya.kumar@webdunia.net>
2020-12-17 13:24:10 +0530

    Property Binding

--- src/app/basic-things/basic-things.component.ts
+++ src/app/basic-things/basic-things.component.ts
@@ -5,11 +5,18 @@ import { Component, OnInit } from "@angular/core";
   templateUrl: "./basic-things.component.html",
   styleUrls: ["./basic-things.component.css"],
 })
-export class BasicThingsComponent {
+export class BasicThingsComponent implements OnInit {
   string1: number = 100;
   string2: string = "bucks";
-
-  howManyMoney(){
+  isdisabled: boolean = false;
+  howManyMoney() {
     return this.string1;
   }
+
+  constructor() {
+    setTimeout(() => {
+      this.isdisabled = true;
+    }, 2000);
+  }
+  ngOnInit() {}
 }
